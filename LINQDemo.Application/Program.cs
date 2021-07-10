3b22d070b51b274b41ff95130fd15b2540926f1f
shelle mohammed olatunji <shelledotnet@outlook.com>
2021-07-10 11:05:21 +0100

    added people json file

---
+++ LINQDemo.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQDemo.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
